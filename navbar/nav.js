69342e86f1f66605c343e21a74df8eaccdef0c46
ryguy0601 <84422347+ryguy0601@users.noreply.github.com>
2024-04-04 08:41:18 -0400

    working nav

--- navbar/nav.js
+++ navbar/nav.js
@@ -1,7 +1,20 @@
-
 document.getElementsByTagName(
 	"head"
 )[0].innerHTML += `<link rel="stylesheet" href="/navbar/nav.css">`;
+
+
+document.getElementById("nav").innerHTML=`
+<nav class="navbar">
+	<a href="/index.html">Home</a>
+	<div class="nav-dropdown">
+		<button class="nav-dropbtn">Games</button>
+		<div class="nav-dropdown-content">
+			<a href="/2048/2048.html">2048</a>
+			<a href="#">WIP</a>
+		</div>
+	</div>
+</nav>
+`
 
 
 // fetch("/navbar/nav.html")
@@ -15,17 +28,17 @@ document.getElementsByTagName(
 // 	});
 
 // console.log(1)
-(async function(){
-	let code = await fetch("https://ryguy0601.github.io/myWebsite/navbar/nav.js").then((response) => response.text())
-	let location = document.getElementById("nav")
-	location.innerHTML = code;
-})();
+// (async function(){
+// 	let code = await fetch("https://ryguy0601.github.io/myWebsite/navbar/nav.js").then((response) => response.text())
+// 	let location = document.getElementById("nav")
+// 	location.innerHTML = code;
+// })();
 
-	// .then((response) => response.text())
-	// .then((data) => {
-	// 	document.getElementById("nav").innerHTML = data;
-	// 	console.log(data)
-	// })
-	// .catch((error) => {
-	// 	console.error("Error:", error);
-	// });
+// .then((response) => response.text())
+// .then((data) => {
+// 	document.getElementById("nav").innerHTML = data;
+// 	console.log(data)
+// })
+// .catch((error) => {
+// 	console.error("Error:", error);
+// });
