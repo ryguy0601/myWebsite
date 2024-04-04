f0b99f8c8f9237856a4daabaaddd452b18b31b61
ryguy0601 <84422347+ryguy0601@users.noreply.github.com>
2024-04-04 08:28:19 -0400

    please let the nav work

--- navbar/nav.js
+++ navbar/nav.js
@@ -4,14 +4,28 @@ document.getElementsByTagName(
 )[0].innerHTML += `<link rel="stylesheet" href="/navbar/nav.css">`;
 
 
-fetch("/navbar/nav.html")
-	.then((response) => response.text())
-	.then((data) => {
-		document.getElementById("nav").innerHTML = data;
-		console.log(data)
-	})
-	.catch((error) => {
-		console.error("Error:", error);
-	});
+// fetch("/navbar/nav.html")
+// 	.then((response) => response.text())
+// 	.then((data) => {
+// 		document.getElementById("nav").innerHTML = data;
+// 		console.log(data)
+// 	})
+// 	.catch((error) => {
+// 		console.error("Error:", error);
+// 	});
 
-console.log(1)
+// console.log(1)
+(async function(){
+	let code = await fetch("/navbar/nav.html").then((response) => response.text())
+	let location = document.getElementById("nav")
+	location.innerHTML = code;
+})();
+
+	// .then((response) => response.text())
+	// .then((data) => {
+	// 	document.getElementById("nav").innerHTML = data;
+	// 	console.log(data)
+	// })
+	// .catch((error) => {
+	// 	console.error("Error:", error);
+	// });
