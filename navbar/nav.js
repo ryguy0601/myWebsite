4509c3d5599b6c8b5193f59e9585985d8f2f1863
ryguy0601 <84422347+ryguy0601@users.noreply.github.com>
2024-04-05 09:40:03 -0400

    hopfally final test

--- navbar/nav.js
+++ navbar/nav.js
@@ -1,15 +1,19 @@
+
+location = document.location
+
+
 document.getElementsByTagName(
 	"head"
-)[0].innerHTML += `<link rel="stylesheet" href="navbar/nav.css">`;
+)[0].innerHTML += `<link rel="stylesheet" href=`+location+`"/navbar/nav.css">`;
 
 
 document.getElementById("nav").innerHTML=`
 <nav class="navbar">
-	<a href="/index.html">Home</a>
+	<a href=`+location+`"/index.html">Home</a>
 	<div class="nav-dropdown">
 		<button class="nav-dropbtn">Games</button>
 		<div class="nav-dropdown-content">
-			<a href="/2048/2048.html">2048</a>
+			<a href=`+location+`"/2048/2048.html">2048</a>
 			<a href="#">WIP</a>
 		</div>
 	</div>
