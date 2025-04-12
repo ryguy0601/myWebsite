32b2eeb744fb472b13ec399231654ff16565ff65
ryguy0601 <84422347+ryguy0601@users.noreply.github.com>
2025-04-12 15:28:32 -0400

    add navigation dropdown for About Me section and social links in footer

--- navbar/nav.js
+++ navbar/nav.js
@@ -3,22 +3,6 @@ document.getElementsByTagName(
 )[0].innerHTML += `<link rel="stylesheet" href="../navbar/nav.css">`;
 
 
-// document.getElementById("nav").innerHTML= nav.html
-
-// `
-// <nav class="navbar">
-// 	<a href="/main/index.html">Home</a>
-// 	<div class="nav-dropdown">
-// 		<button class="nav-dropbtn">Games</button>
-// 		<div class="nav-dropdown-content">
-// 			<a href="/2048/2048.html">2048</a>
-// 			<a href="/chess/chess.html">Chess(WIP)</a>
-// 		</div>
-// 	</div>
-// </nav>
-// `
-
-
 fetch("../navbar/nav.html")
 	.then((response) => response.text())
 	.then((data) => {
@@ -30,18 +14,3 @@ fetch("../navbar/nav.html")
 		console.error("Error:", error);
 	});
 
-// console.log(1)
-// (async function(){
-// 	let code = await fetch("https://ryguy0601.github.io/myWebsite/navbar/nav.js").then((response) => response.text())
-// 	let location = document.getElementById("nav")
-// 	location.innerHTML = code;
-// })();
-
-// .then((response) => response.text())
-// .then((data) => {
-// 	document.getElementById("nav").innerHTML = data;
-// 	console.log(data)
-// })
-// .catch((error) => {
-// 	console.error("Error:", error);
-// });
